7a9e18f02b704fc53c40e1f9fa23ecaeaedf1b8f
Luzefiru <ian.deejay.iv@gmail.com>
2025-01-27 00:15:41 +0800

    chore: clean up tsc remnants

--- src/config/env.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.env = void 0;
-exports.env = {
-    DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN,
-    TENOR_API_KEY: process.env.TENOR_API_KEY,
-};
